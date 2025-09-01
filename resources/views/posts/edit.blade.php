82cd2e969279e907ab1762b5abed6380e74b298d
João Vitor da Costa Andrade <joaovcostaandrade@gmail.com>
2025-09-01 19:20:13 -0300

    Edit ajuste

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,22 +1,54 @@
 @extends('layout')
 
 @section('content')
-    <h1>Edit Posts</h1>
-    <form action="{{ route('posts.update', $post->id) }}" method="PUT">
+    <h1>Editar Post</h1>
+
+    @if ($errors->any())
+        <div style="color:#b00; margin-bottom:1rem;">
+            <ul>
+                @foreach ($errors->all() as $err)
+                    <li>{{ $err }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('posts.update', $post) }}" method="POST">
         @csrf
+        @method('PUT')
+
         <div>
             <label for="title">Title</label>
-            <input type="text" id="title" name="title" value="{{ $post->title }}" required>
+            <input
+                type="text"
+                id="title"
+                name="title"
+                value="{{ old('title', $post->title) }}"
+                required
+            >
         </div>
+
         <div>
             <label for="content">Content</label>
-            <textarea id="content" name="content" required>{{ $post->content }}</textarea>
+            <textarea
+                id="content"
+                name="content"
+                required
+            >{{ old('content', $post->content) }}</textarea>
         </div>
+
         <div>
             <label for="author">Author</label>
-            <input type="text" id="author" name="author" value="{{ $post->author }}" required>
+            <input
+                type="text"
+                id="author"
+                name="author"
+                value="{{ old('author', $post->author) }}"
+                required
+            >
         </div>
-        <button type="submit">Update Post</button>
+
+        <button type="submit">Salvar alterações</button>
+        <a href="{{ route('posts.show', $post) }}">Cancelar</a>
     </form>
 @endsection
-
